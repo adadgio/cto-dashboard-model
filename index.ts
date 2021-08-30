486dc9401381b14110344c579f99d253f28523c5
Skeller <keller.samuel1@gmail.com>
2021-08-30 16:42:27 +0200

    removed default

--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ import Issue from "./src/classes/Issue";
 import Project from "./src/classes/Project";
 import Sprint from "./src/classes/Sprint";
 
-export default { Issue, Project, Sprint };
+export { Issue, Project, Sprint };
