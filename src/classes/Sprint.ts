222c421074b128b0ad25a6309ccbe8ed15768347
L. V <5218455-l.v.simplon@users.noreply.gitlab.com>
2021-09-01 19:58:08 +0200

    add Issue:dates

--- src/classes/Sprint.ts
+++ src/classes/Sprint.ts
@@ -2,16 +2,27 @@ interface SprintConstructorType {
   id: number;
   boardId: number;
   name: string;
+
+  startDate?: string,
+  endDate?: string,
+  completeDate?: string,
 }
 
 export default class Sprint {
   id!: number;
   boardId!: number;
   name!: string;
+  startDate?: string,
+  endDate?: string,
+  completeDate?: string,
 
   constructor(data: SprintConstructorType) {
     this.id = data.id;
     this.boardId = data.boardId;
     this.name = data.name;
+
+    this.startDate = data.startDate;
+    this.endDate = data.endDate;
+    this.completeDate = data.completeDate;
   }
 }
